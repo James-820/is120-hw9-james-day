30df10a0e37fb6fcfc6f88f81afc92e4dbe2ccb3
James Day <daj4102@byu.edu>
2026-03-18 20:22:28 -0600

    Checkpoint. Got layout mostly done, now working on JS

---
+++ script.js
@@ -0,0 +1,19 @@
+// Declare global variables (elements as variables):
+// ===========================================================================
+
+let input = document.querySelector("#input");
+let tipOutput = document.querySelector("#tip-result");
+let totalOutput = document.querySelector("#total-result");
+
+// Automatically set 15% as selected by default:
+document.querySelector("#rad1").checked = true;
+
+// Define functions for event listeners:
+// ===========================================================================
+
+// Update the outputs:
+
+// Update radio button selection:
+
+// Add event listeners:
+// ===========================================================================
