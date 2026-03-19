195bcf2669ae9a3eeba298c93ed29994e3da79f5
James Day <daj4102@byu.edu>
2026-03-18 20:46:57 -0600

    Started with JS logic. Doesn't work yet

--- script.js
+++ script.js
@@ -4,16 +4,54 @@
 let input = document.querySelector("#input");
 let tipOutput = document.querySelector("#tip-result");
 let totalOutput = document.querySelector("#total-result");
+let rad1 = document.querySelector("#rad1");
+let rad2 = document.querySelector("#rad2");
+let rad3 = document.querySelector("#rad3");
+let rad4 = document.querySelector("#rad4");
+let custom = document.querySelector("#custom-percent");
 
 // Automatically set 15% as selected by default:
-document.querySelector("#rad1").checked = true;
+rad1.checked = true;
 
-// Define functions for event listeners:
+let inAmount = parseFloat(input.value);
+
+// Define function(s) for event listeners:
 // ===========================================================================
 
-// Update the outputs:
-
-// Update radio button selection:
+function updateOutputs() {
+  let percent;
+  let tipResult;
+  let totResult;
+  // Determine the checked button:
+  if (rad1.checked) {
+    percent = 0.15;
+  } else if (rad2.checked) {
+    percent = 0.2;
+  } else if (rad3.checked) {
+    percent = 0.25;
+  } else {
+    // Get custom value:
+    // (Early return if there's no value)
+    if (custom.value == "") {
+      return;
+    }
+    // Get the percent by dividing by 100:
+    percent = parseFloat(custom.value) / 100;
+  }
+  // Get the tip amount and total amounts:
+  tipResult = inAmount * percent;
+  totResult = tipResult + inAmount;
+  // Write them to the DOM elements:
+  tipOutput.textContent = tipResult;
+  totalOutput.textContent = totResult;
+}
 
 // Add event listeners:
 // ===========================================================================
+
+input.addEventListener("input", updateOutputs);
+rad1.addEventListener("click", updateOutputs);
+rad2.addEventListener("click", updateOutputs);
+rad3.addEventListener("click", updateOutputs);
+rad4.addEventListener("click", updateOutputs);
+custom.addEventListener("input", updateOutputs);
